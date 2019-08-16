97b0438c788039ce090bc232521d27b1ea7a6c5b
fabod <2189668+fabod@users.noreply.github.com>
2019-08-16 14:36:40 +0200

    fixed baseclass problem

--- orbis_addon_repoman/format/nif/main.py
+++ orbis_addon_repoman/format/nif/main.py
@@ -3,10 +3,10 @@
 from . import convert
 from orbis_eval import app
 
+
 def run(file_destination, corpus_dir, file_name):
 
     if file_name.endswith("ttl"):
         convert.Convert().convert(file_destination, corpus_dir, file_name)
     else:
         convert.Convert().convert(file_destination, corpus_dir, file_name)
-
