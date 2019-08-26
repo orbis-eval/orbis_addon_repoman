d5ebd3a01e2296de8c40a5305ce474bdcd1642b7
fabod <2189668+fabod@users.noreply.github.com>
2019-08-26 10:40:19 +0200

    Added support for ttl files that use itsrdf:taClassRef as predicate to link between entity and entity-type.

--- orbis_addon_repoman/format/nif/convert.py
+++ orbis_addon_repoman/format/nif/convert.py
@@ -39,13 +39,14 @@ class Convert(object):
         with open(os.path.join(folder, file_name), "w") as open_file:
 
             for subject, predicate, object_ in g.triples((None, self.nif_namespace.anchorOf, None)):
+
                 """
-                print(f"\n")
+                print("\n", 42 * "", "\n")
                 print(f"subject: {subject}")
                 print(f"predicate: {predicate}")
                 print(f"object: {object_}")
                 print(f"\n")
-                """
+                # """
 
                 subject_id = subject.split("/")[-1]
                 document_number, postition = subject_id.split("#")
@@ -56,6 +57,59 @@ class Convert(object):
                 for subject_2, predicate_2, object_2 in g.triples((subject, self.itsrdf_namespace.taIdentRef, None)):
                     type_ = dbpedia_entity_types.get_dbpedia_type(object_2)
 
+                    """
+                    print(f"\n")
+                    print(f"subject_2: {subject_2}")
+                    print(f"predicate_2: {predicate_2}")
+                    print(f"object_3: {object_2}")
+                    print(f"\n")
+                    # """
+
                     line = "\t".join([document_number, start, end, object_2.strip(), "1", type_, surfaceForm])
                     open_file.write(line + "\n")
 
+                for subject_2, predicate_2, object_2 in g.triples((subject, self.itsrdf_namespace.taClassRef, None)):
+
+                    """
+                    print(f"\n")
+                    print(f"subject_2: {subject_2}")
+                    print(f"predicate_2: {predicate_2}")
+                    print(f"object_3: {object_2}")
+                    print(f"\n")
+                    # """
+
+                    types = ['http://dbpedia.org/ontology/Person', 'http://xmlns.com/foaf/0.1/Person', 'http://dbpedia.org/ontology/Organisation', 'http://dbpedia.org/class/yago/Organization108008335', 'http://dbpedia.org/ontology/PopulatedPlace', 'http://dbpedia.org/ontology/Place', 'http://dbpedia.org/ontology/TelevisionShow', 'http://dbpedia.org/ontology/Work', 'http://dbpedia.org/ontology/Work', 'http://www.w3.org/2002/07/owl#Thing']
+                    if object_2.strip() in types:
+                        type_ = self.define_type(object_2)
+
+                        line = self.write_entity_to_file(
+                            open_file,
+                            document_number,
+                            start,
+                            end,
+                            object_2,
+                            type_,
+                            surfaceForm
+                        )
+                        # print(line)
+
+    def write_entity_to_file(self, open_file, document_number, start, end, object_2, type_, surfaceForm, separator="\t"):
+        line = "\t".join([document_number, start, end, object_2.strip(), "1", type_, surfaceForm])
+        open_file.write(line + "\n")
+        return line
+
+    def define_type(self, object_):
+
+        types = {
+            'http://dbpedia.org/ontology/Person': 'Person',
+            'http://xmlns.com/foaf/0.1/Person': 'Person',
+            'http://dbpedia.org/ontology/Organisation': 'Organization',
+            'http://dbpedia.org/class/yago/Organization108008335': 'Organization',
+            'http://dbpedia.org/ontology/PopulatedPlace': 'Location',
+            'http://dbpedia.org/ontology/Place': 'Location',
+            'http://dbpedia.org/ontology/TelevisionShow': 'TV',
+            'http://dbpedia.org/ontology/Work': 'TV',
+            'http://dbpedia.org/ontology/Work': 'Work',
+        }
+
+        return types.get(object_.strip(), 'http://www.w3.org/2002/07/owl#Thing')
