f3a0427588c1345eb46b7cbd7beceaaf5c3722d6
roger <climberwoodi@gmx.ch>
2021-02-19 07:57:05 +0100

    chg: updated author info.

--- orbis_addon_repoman/__init__.py
+++ orbis_addon_repoman/__init__.py
@@ -2,8 +2,8 @@
 
 __name__ = "orbis_addon_repoman"
 __version__ = "2.3.0"
-__author__ = "fabod"
-__year__ = "2019"
+__author__ = "fabod, Roger Waldvogel"
+__year__ = "2021"
 __description__ = "The Repoman Addon for Orbis to download and install Corpora and Monocle files."
 __license__ = "GPL2"
 __min_python_version__ = "3.6"
